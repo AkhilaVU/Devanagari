bf1781b23d13ba1fe22f403569efb6fedef73039
AkhilaVU <vuakhila@gmail.com>
2019-04-23 11:06:44 +0530

    Devanagari digit recognition

--- recognition.py
+++ recognition.py
@@ -1,37 +1,57 @@
-import numpy as np
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing import image
-from keras.models import load_model
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
 
-# Data Preprocessing
-train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
-training_set = train_datagen.flow_from_directory('cnn/dataset/training_set', color_mode='grayscale',target_size=(32, 32), batch_size=32, class_mode='categorical')
-test_set = test_datagen.flow_from_directory('cnn/dataset/test_set', color_mode='grayscale', target_size=(32, 32), batch_size=32, class_mode='categorical')
-
-# Initialize the CNN
 classifier = Sequential()
 
-# Add Convolution2D layer 
-classifier.add(Conv2D(32, (3, 3), input_shape = (32, 32, 1), activation='relu'))
-
-# Pooling
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-classifier.add(Conv2D(32, (3, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-classifier.add(Conv2D(32, (3, 3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-# Flattening
+classifier = Sequential()
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Flatten())
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 10, activation = 'softmax'))
 
-# Fully connected network
-classifier.add(Dense(units=128, activation='relu'))
-classifier.add(Dense(units=128, activation='relu'))
-classifier.add(Dense(units=64, activation='relu'))
-classifier.add(Dense(units=46, activation='softmax'))
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+shear_range = 0.2,
+zoom_range = 0.2,
+horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+ training_set = train_datagen.flow_from_directory('train',
+target_size = (64, 64),
+batch_size = 32,
+class_mode = 'categorical')
+
+ test_set = test_datagen.flow_from_directory('test',
+target_size = (64, 64),
+batch_size = 32,
+class_mode = 'categorical')
+
+classifier.fit_generator(training_set,
+steps_per_epoch = 17000,
+epochs = 2,
+validation_data = test_set,
+validation_steps = 3000)
+
+import numpy as np
+from keras.preprocessing import image
+
+individual_image=image.load_img('second.png', target_size = (64, 64))
+individual_image = image.img_to_array(individual_image)
+individual_image = np.expand_dims(individual_image, axis = 0)
+result = classifier.predict(individual_image)
+result
+
+training_set.class_indices
+
+individual_image=image.load_img('image.jpg', target_size = (64, 64))
+individual_image = image.img_to_array(individual_image)
+individual_image = np.expand_dims(individual_image, axis = 0)
+result = classifier.predict(individual_image)
+result
